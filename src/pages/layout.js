88b27182b1bf7a7a72ff5edc8c3546725e9ae7be
qrusadorz <qrusadorz@gmail.com>
2019-01-03 21:56:16 +0900

    add fotter.

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import withRoot from '../withRoot';
 
@@ -10,6 +11,7 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase, { uiConfig } from '../firebase/firebase'
 
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 import SwipeableTemporaryDrawer from '../components/SwipeableTemporaryDrawer'
 import MenuAppBar from '../components/menuappbar';
@@ -26,12 +28,41 @@ const styles = theme => ({
     // TODO original test
     appBarSpacer: theme.mixins.toolbar,
     content: {
-      flexGrow: 1,
-      padding: theme.spacing.unit * 3,
-      height: '100vh',
-      overflow: 'auto',
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+        height: '100vh',
+        overflow: 'auto',
+    },
+
+    // footer: {
+    //     backgroundColor: theme.palette.background.paper,
+    //     padding: theme.spacing.unit * 6,
+    // },
+    footer: {
+        marginTop: theme.spacing.unit * 8,
+        borderTop: `1px solid ${theme.palette.divider}`,
+        padding: `${theme.spacing.unit * 6}px 0`,
     },
 });
+
+const footers = [
+    {
+      title: 'Company',
+      description: ['Team', 'History', 'Contact us', 'Locations'],
+    },
+    {
+      title: 'Features',
+      description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
+    },
+    {
+      title: 'Resources',
+      description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    },
+    {
+      title: 'Legal',
+      description: ['プライバシー', '利用規約'],
+    },
+  ];
 
 class Layout extends React.Component {
     constructor(props) {
@@ -135,6 +166,35 @@ class Layout extends React.Component {
                         <AlignItemsList />
                         {/* <Qrcode /> */}
                         <SimpleSnackBar />
+                        {/* mainから出したいがスクロール問題でNG */}
+                        {/* Footer */}
+                        {/* <footer className={classes.footer}>
+                            <Typography variant="h6" align="center" gutterBottom>
+                                Footer
+                            </Typography>
+                            <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                                Something here to give the footer a purpose!
+                            </Typography>
+                        </footer> */}
+                        {/* End footer */}
+                        {/* Footer */}
+                        <footer className={classNames(classes.footer, classes.layout)}>
+                            <Grid container spacing={32} justify="space-evenly">
+                            {footers.map(footer => (
+                                <Grid item xs key={footer.title}>
+                                <Typography variant="h6" color="textPrimary" gutterBottom>
+                                    {footer.title}
+                                </Typography>
+                                {footer.description.map(item => (
+                                    <Typography key={item} variant="subtitle1" color="textSecondary">
+                                    {item}
+                                    </Typography>
+                                ))}
+                                </Grid>
+                            ))}
+                            </Grid>
+                        </footer>
+                        {/* End footer */}
                     </main>
                 </UserContext.Provider>
             </SystemContext.Provider>
