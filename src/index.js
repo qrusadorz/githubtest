f43a3f7d12aa6eeb5e9e8e26a8795f6785a6e6f6
qrusadorz <qrusadorz@gmail.com>
2019-02-11 23:41:29 +0900

    disable log in production enviroment.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Index from './pages/index';
 import * as serviceWorker from './serviceWorker';
 
+// disable the production enviroment log
+process.env.NODE_ENV !== "development" && (console.log = () => {});
+
 ReactDOM.render(<Index />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
