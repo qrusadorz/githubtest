430a491e2fda19d625fd0694ba23f370de7c1435
qrusadorz <qrusadorz@gmail.com>
2019-02-15 19:39:47 +0900

    enable service worker.

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ ReactDOM.render(<Index />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
 // TODO ServiceWorkerを有効にする際に解除
-// serviceWorker.register();
+serviceWorker.register();
