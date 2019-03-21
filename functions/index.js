ed1708af79a6df801d50f5fdfc624e0bfec25074
qrusadorz <qrusadorz@gmail.com>
2019-03-21 22:37:21 +0900

    prepare to call functions.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,21 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.feedback = functions.https.onCall((data, context) => {
+    const { text = "" } = data;
+
+    // Checking attribute.
+    if (!(typeof text === 'string') || text.length === 0) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+            'one arguments "text" containing the message text to add.');
+    }
+    
+    // // Authentication / user information is automatically added to the request.
+    // const uid = context.auth.uid;
+    // const name = context.auth.token.name || null;
+    // const picture = context.auth.token.picture || null;
+    // const email = context.auth.token.email || null;
+    return {};
+});
