a6d69f02e10b46822a84d4cd180f8817be0f8421
qrusadorz <qrusadorz@gmail.com>
2019-03-22 09:16:56 +0900

    replace with cloudstore for a while

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,11 @@
 const functions = require('firebase-functions');
 // Import and initialize the Firebase Admin SDK.
-// const admin = require('firebase-admin');
-// admin.initializeApp();
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const { WebClient } = require('@slack/client');
+
+const { token, conversationId } = require('./configs/slack');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -10,8 +14,23 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
+// An access token (from your Slack app or custom integration - xoxp, xoxb)
+// const token = process.env.SLACK_TOKEN;
+
+// const web = new WebClient(token);
+
+// This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
+
+
+const db = admin.firestore();
+
+const setFeedback = async (json) => {
+    // await db.collection("items").doc("latest").update(items);
+    await db.collection("feedback").add(json);
+}
+
 const regionTokyo = 'asia-northeast1';
-exports.feedback = functions.region(regionTokyo).https.onCall((data, context) => {
+exports.feedback = functions.region(regionTokyo).https.onCall(async (data, context) => {
     const { text = "" } = data;
 
     // Checking attribute.
@@ -26,5 +45,15 @@ exports.feedback = functions.region(regionTokyo).https.onCall((data, context) =>
     // const name = context.auth.token.name || null;
     // const picture = context.auth.token.picture || null;
     // const email = context.auth.token.email || null;
+
+    // IMPORTANT: Error by Firebase plan. because limit network by spark plan.
+    // See: https://api.slack.com/methods/chat.postMessage
+    // const res = await web.chat.postMessage({ channel: conversationId, text: 'Hello there from functions' });
+    // `res` contains information about the posted message
+    // console.log('Message sent: ', res.ts);
+
+    // TODO replace db for a while.
+    await setFeedback({text});
+
     return { text };
 });
