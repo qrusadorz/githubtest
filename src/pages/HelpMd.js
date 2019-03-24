ffcc54fd9d99794eeba453bfe0dece681d347a5f
qrusadorz <qrusadorz@gmail.com>
2019-03-24 15:51:41 +0900

    change to lazy load the markdown files

---
+++ src/pages/HelpMd.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+import Markdown from '../components/Markdown';
+import raw from "raw.macro";
+
+const styles = theme => ({
+    markdown: {
+        padding: `${theme.spacing.unit * 3}px 0`,
+        // for buttom navigation.
+        marginBottom: theme.spacing.unit * 6,
+      },
+    });
+
+const md = raw('../articles/help.md');
+
+function HelpMd(props) {
+    const { classes } = props;
+    return <Markdown className={classes.markdown}>{md}</Markdown>;
+  }
+
+export default withStyles(styles)(HelpMd);
